209e901e0587569bb62870e535b42cb96f162dab
TIBkey <81963789+baokeyit1177key@users.noreply.github.com>
2025-10-06 22:36:36 +0700

    UPDATE ENTITY Product

--- src/main/java/org/example/audio_ecommerce/service/ProductService.java
+++ src/main/java/org/example/audio_ecommerce/service/ProductService.java
@@ -1,16 +1,16 @@
-package org.example.audio_ecommerce.service;
-
-import org.example.audio_ecommerce.dto.request.CreateProductRequest;
-import org.example.audio_ecommerce.dto.request.UpdateProductRequest;
-import org.example.audio_ecommerce.entity.Product;
-
-import java.util.List;
-import java.util.UUID;
-
-public interface ProductService {
-    Product createProduct(CreateProductRequest request);
-    List<Product> getAllProducts();
-    Product getProductById(UUID id);
-    Product updateProduct(UUID id, UpdateProductRequest request);
-    void deleteProduct(UUID id);
-}
+//package org.example.audio_ecommerce.service;
+//
+//import org.example.audio_ecommerce.dto.request.CreateProductRequest;
+//import org.example.audio_ecommerce.dto.request.UpdateProductRequest;
+//import org.example.audio_ecommerce.entity.Product;
+//
+//import java.util.List;
+//import java.util.UUID;
+//
+//public interface ProductService {
+//    Product createProduct(CreateProductRequest request);
+//    List<Product> getAllProducts();
+//    Product getProductById(UUID id);
+//    Product updateProduct(UUID id, UpdateProductRequest request);
+//    void deleteProduct(UUID id);
+//}
