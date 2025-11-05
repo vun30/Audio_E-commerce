1ba253a2333bf2183831c122f99857d42830a8a3
vun30 <hoanvu3012@gmail.com>
2025-11-05 23:26:27 +0700

    update get all staff for store

--- src/main/java/org/example/audio_ecommerce/repository/StaffRepository.java
+++ src/main/java/org/example/audio_ecommerce/repository/StaffRepository.java
@@ -1,9 +1,14 @@
 package org.example.audio_ecommerce.repository;
 
 import org.example.audio_ecommerce.entity.Staff;
+import org.example.audio_ecommerce.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StaffRepository extends JpaRepository<Staff, UUID> {
+    List<Staff> findByStore(Store store);
+    Optional<Staff> findByIdAndStore(UUID id, Store store);
 }
--- src/main/java/org/example/audio_ecommerce/service/StaffService.java
+++ src/main/java/org/example/audio_ecommerce/service/StaffService.java
@@ -3,9 +3,12 @@ package org.example.audio_ecommerce.service;
 import org.example.audio_ecommerce.dto.request.StaffCreateRequest;
 import org.example.audio_ecommerce.dto.response.StaffResponse;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface StaffService {
     StaffResponse createStaff(UUID storeId, StaffCreateRequest request);
+    List<StaffResponse> getAllStaffByStoreId(UUID storeId);
+    StaffResponse getStaffById(UUID storeId, UUID staffId);
 }
 
