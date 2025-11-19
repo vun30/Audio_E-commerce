d354618ae554e7c3c94d00d0b954e663148d2523
vun30 <hoanvu3012@gmail.com>
2025-11-19 17:02:30 +0700

    update get order detail for store

--- src/main/java/org/example/audio_ecommerce/service/StoreOrderService.java
+++ src/main/java/org/example/audio_ecommerce/service/StoreOrderService.java
@@ -2,6 +2,7 @@ package org.example.audio_ecommerce.service;
 
 import org.example.audio_ecommerce.dto.response.PagedResult;
 import org.example.audio_ecommerce.dto.response.StoreOrderDetailResponse;
+import org.example.audio_ecommerce.dto.response.StoreOrderResponse;
 import org.example.audio_ecommerce.entity.Enum.OrderStatus;
 import org.example.audio_ecommerce.entity.StoreOrder;
 import java.util.UUID;
@@ -9,5 +10,6 @@ import java.util.UUID;
 public interface StoreOrderService {
     StoreOrder updateOrderStatus(UUID storeId, UUID orderId, OrderStatus status);
     PagedResult<StoreOrderDetailResponse> getOrdersForStore(UUID storeId, int page, int size);
+    StoreOrderDetailResponse getOrderDetailForStore(UUID storeId, UUID orderId);
 }
 
