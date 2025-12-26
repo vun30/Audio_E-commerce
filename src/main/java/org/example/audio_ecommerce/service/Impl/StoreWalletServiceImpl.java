ecbdbaa94a986a550967ef159a37fc12f6ea15fb
TiBkey <baoyasuohoang@gmail.com>
2025-12-27 02:52:22 +0700

    fix(wallet): update platform wallet handling to avoid cash balance modification

--- src/main/java/org/example/audio_ecommerce/service/Impl/StoreWalletServiceImpl.java
+++ src/main/java/org/example/audio_ecommerce/service/Impl/StoreWalletServiceImpl.java
@@ -362,7 +362,7 @@ public class StoreWalletServiceImpl implements StoreWalletService {
                 .balanceAfter(platformAfter)
 
                 // audit
-                .description("Cửa hàng đã thanh toán khoản nợ phí vận chuyển, khoản tiền lưu hành trong hệ thống !!" + tx.getTransactionId())
+                .description("Cửa hàng đã thanh toán khoản nợ phí vận chuyển, khoản tiền lưu hành trong hệ thống !!" )
                 .createdAt(now)
                 .updatedAt(now)
                 .build();
